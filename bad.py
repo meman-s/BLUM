b0c74aaee57957c0545ccb1e57e1728ee7b96a35
meman_s <stepan.kalimullin@gmail.com>
2025-02-06 08:07:09 +0300

    long

--- bad.py
+++ bad.py
@@ -4,3 +4,4 @@ def jfed():
     return math.tan(math.pi/4)
 def jfed2():
     return math.tan(math.pi/2)
+print("fsdfsdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
